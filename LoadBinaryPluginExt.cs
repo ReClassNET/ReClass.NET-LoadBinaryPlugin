4cc556e13c49796d0cbea35877de5a40d82ef2d6
KN4CK3R <admin@oldschoolhack.me>
2016-12-20 01:45:25 +0100

    Sync with ReClass.NET changes.

--- LoadBinaryPluginExt.cs
+++ LoadBinaryPluginExt.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.IO.MemoryMappedFiles;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using ReClassNET.Core;
+using ReClassNET.Debugger;
 using ReClassNET.Memory;
 using ReClassNET.Plugins;
-using RGiesecke.DllExport;
-using static ReClassNET.Memory.NativeHelper;
 
 namespace LoadBinaryPlugin
 {
-	public class LoadBinaryPluginExt : Plugin
+	public class LoadBinaryPluginExt : Plugin, ICoreProcessFunctions
 	{
-		private static object sync = new object();
+		private object sync = new object();
 
-		private static IPluginHost host;
+		private IPluginHost host;
 
-		private static string currentFile;
+		private string currentFile;
 
-		private static Dictionary<IntPtr, MemoryMappedFile> openFiles;
+		private Dictionary<IntPtr, MemoryMappedFile> openFiles;
 
 		public override Image Icon => Properties.Resources.icon;
 
@@ -35,7 +34,9 @@ namespace LoadBinaryPlugin
 				throw new ArgumentNullException(nameof(host));
 			}
 
-			LoadBinaryPluginExt.host = host;
+			this.host = host;
+
+			host.Process.CoreFunctions.RegisterFunctions("Load Binary", this);
 
 			openFiles = new Dictionary<IntPtr, MemoryMappedFile>();
 
@@ -56,7 +57,7 @@ namespace LoadBinaryPlugin
 		/// <summary>Gets a <see cref="MemoryMappedFile"/> by its plugin internal identifier.</summary>
 		/// <param name="id">The identifier.</param>
 		/// <returns>The file or null if the identifier doesn't exist.</returns>
-		private static MemoryMappedFile GetMappedFileById(IntPtr id)
+		private MemoryMappedFile GetMappedFileById(IntPtr id)
 		{
 			MemoryMappedFile file;
 			openFiles.TryGetValue(id, out file);
@@ -66,7 +67,7 @@ namespace LoadBinaryPlugin
 		/// <summary>Logs the exception and removes the file.</summary>
 		/// <param name="id">The identifier.</param>
 		/// <param name="ex">The exception.</param>
-		private static void LogErrorAndRemoveFile(IntPtr id, Exception ex)
+		private void LogErrorAndRemoveFile(IntPtr id, Exception ex)
 		{
 			Contract.Requires(ex != null);
 
@@ -84,8 +85,7 @@ namespace LoadBinaryPlugin
 		/// <summary>Queries if the file is valid.</summary>
 		/// <param name="process">The file to check.</param>
 		/// <returns>True if the file is valid, false if not.</returns>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		public static bool IsProcessValid(IntPtr process)
+		public bool IsProcessValid(IntPtr process)
 		{
 			lock (sync)
 			{
@@ -97,8 +97,7 @@ namespace LoadBinaryPlugin
 		/// <param name="pid">The file id.</param>
 		/// <param name="desiredAccess">The desired access. (ignored)</param>
 		/// <returns>A plugin internal handle to the file.</returns>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static IntPtr OpenRemoteProcess(IntPtr id, ProcessAccess desiredAccess)
+		public IntPtr OpenRemoteProcess(IntPtr id, ProcessAccess desiredAccess)
 		{
 			lock (sync)
 			{
@@ -126,8 +125,7 @@ namespace LoadBinaryPlugin
 
 		/// <summary>Closes the file.</summary>
 		/// <param name="process">The file to close.</param>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static void CloseRemoteProcess(IntPtr process)
+		public void CloseRemoteProcess(IntPtr process)
 		{
 			lock (sync)
 			{
@@ -144,11 +142,11 @@ namespace LoadBinaryPlugin
 		/// <summary>Reads memory of the file.</summary>
 		/// <param name="process">The process to read from.</param>
 		/// <param name="address">The address to read from.</param>
-		/// <param name="buffer">The buffer to read into.</param>
+		/// <param name="buffer">[out] The buffer to read into.</param>
+		/// <param name="offset">The offset into the buffer.</param>
 		/// <param name="size">The size of the memory to read.</param>
 		/// <returns>True if it succeeds, false if it fails.</returns>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static bool ReadRemoteMemory(IntPtr process, IntPtr address, IntPtr buffer, int size)
+		public bool ReadRemoteMemory(IntPtr process, IntPtr address, ref byte[] buffer, int offset, int size)
 		{
 			lock (sync)
 			{
@@ -159,10 +157,7 @@ namespace LoadBinaryPlugin
 					{
 						using (var stream = file.CreateViewStream(address.ToInt64(), size))
 						{
-							var tempBuffer = new byte[size];
-							stream.Read(tempBuffer, 0, size);
-
-							Marshal.Copy(tempBuffer, 0, buffer, size);
+							stream.Read(buffer, 0, size);
 
 							return true;
 						}
@@ -184,21 +179,20 @@ namespace LoadBinaryPlugin
 		/// <summary>Not supported.</summary>
 		/// <param name="process">The file to write to.</param>
 		/// <param name="address">The address to write to.</param>
-		/// <param name="buffer">The memory to write.</param>
+		/// <param name="buffer">[in] The memory to write.</param>
+		/// <param name="offset">The offset into the buffer.</param>
 		/// <param name="size">The size of the memory to write.</param>
 		/// <returns>True if it succeeds, false if it fails.</returns>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static bool WriteRemoteMemory(IntPtr process, IntPtr address, IntPtr buffer, int size)
+		public bool WriteRemoteMemory(IntPtr process, IntPtr address, ref byte[] buffer, int offset, int size)
 		{
-			// No write support.
+			// Not supported.
 
 			return false;
 		}
 
 		/// <summary>Opens a file browser dialog and reports the selected file.</summary>
 		/// <param name="callbackProcess">The callback which gets called for the selected file.</param>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static void EnumerateProcesses(EnumerateProcessCallback callbackProcess)
+		public void EnumerateProcesses(Action<Tuple<IntPtr, string>> callbackProcess)
 		{
 			if (callbackProcess == null)
 			{
@@ -213,13 +207,7 @@ namespace LoadBinaryPlugin
 				{
 					currentFile = ofd.FileName;
 
-					var data = new EnumerateProcessData
-					{
-						Id = (IntPtr)currentFile.GetHashCode(),
-						Path = currentFile
-					};
-
-					callbackProcess(ref data);
+					callbackProcess(Tuple.Create((IntPtr)currentFile.GetHashCode(), currentFile));
 				}
 			}
 		}
@@ -228,10 +216,45 @@ namespace LoadBinaryPlugin
 		/// <param name="process">The process.</param>
 		/// <param name="callbackSection">The callback which gets called for every section.</param>
 		/// <param name="callbackModule">The callback which gets called for every module.</param>
-		[DllExport(CallingConvention = CallingConvention.StdCall)]
-		private static void EnumerateRemoteSectionsAndModules(IntPtr process, EnumerateRemoteSectionCallback callbackSection, EnumerateRemoteModuleCallback callbackModule)
+		public void EnumerateRemoteSectionsAndModules(IntPtr process, Action<Section> callbackSection, Action<Module> callbackModule)
 		{
-			// No modules and sections here.
+			// Not supported.
+		}
+
+		public void ControlRemoteProcess(IntPtr process, ControlRemoteProcessAction action)
+		{
+			// Not supported.
+		}
+
+		public bool AttachDebuggerToProcess(IntPtr id)
+		{
+			// Not supported.
+
+			return false;
+		}
+
+		public void DetachDebuggerFromProcess(IntPtr id)
+		{
+			// Not supported.
+		}
+
+		public bool AwaitDebugEvent(ref DebugEvent evt, int timeoutInMilliseconds)
+		{
+			// Not supported.
+
+			return false;
+		}
+
+		public void HandleDebugEvent(ref DebugEvent evt)
+		{
+			// Not supported.
+		}
+
+		public bool SetHardwareBreakpoint(IntPtr id, IntPtr address, HardwareBreakpointRegister register, HardwareBreakpointTrigger trigger, HardwareBreakpointSize size, bool set)
+		{
+			// Not supported.
+
+			return false;
 		}
 	}
 }
